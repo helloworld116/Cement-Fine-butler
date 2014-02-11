fcf72523ce008ec8512859d66dfee4b48b7cd3e9
helloworld116 <646767424@qq.com>
2014-02-11 18:04:03 +0800

    mac mini

---
+++ Cement Fine butler/DropDownView.h
@@ -0,0 +1,21 @@
+//
+//  DropDownView.h
+//  Cement Fine butler
+//
+//  Created by 文正光 on 14-2-11.
+//  Copyright (c) 2014年 河南丰博自动化有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class DropDownView;
+@protocol DropDownViewDeletegate <NSObject>
+
+-(void)dropDownDelegateMethod:(DropDownView *)sender;
+
+@end
+
+@interface DropDownView : UIView<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic,assign) id<DropDownViewDeletegate> delegate;
+-(void)hideDropDown:(UIButton *)btn;
+-(id)showDropDown:(UIButton *)btn height:(CGFloat)height list:(NSArray *)list;
+@end
---
+++ Cement Fine butler/DropDownView.m
@@ -0,0 +1,31 @@
+//
+//  DropDownView.m
+//  Cement Fine butler
+//
+//  Created by 文正光 on 14-2-11.
+//  Copyright (c) 2014年 河南丰博自动化有限公司. All rights reserved.
+//
+
+#import "DropDownView.h"
+
+@implementation DropDownView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
