b0843ac0c3d15ba0ac2eca68b3b7e12269eefd3b
helloworld116 <646767424@qq.com>
2014-03-28 18:02:41 +0800

    mac mini

---
+++ Cement Fine butler/ProductionView.m
@@ -0,0 +1,31 @@
+//
+//  ProductionView.m
+//  Cement Fine butler
+//
+//  Created by 文正光 on 14-3-28.
+//  Copyright (c) 2014年 河南丰博自动化有限公司. All rights reserved.
+//
+
+#import "ProductionView.h"
+
+@implementation ProductionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Cement Fine butler/RealTimeReportVC.h
@@ -0,0 +1,13 @@
+//
+//  RealTimeReportVC.h
+//  Cement Fine butler
+//
+//  Created by 文正光 on 14-3-28.
+//  Copyright (c) 2014年 河南丰博自动化有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RealTimeReportVC : UIViewController
+
+@end
