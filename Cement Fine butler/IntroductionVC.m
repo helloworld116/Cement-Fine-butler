22944ebb74510372335f5662fa3c8fc3c739ffae
helloworld116 <646767424@qq.com>
2014-04-18 17:59:43 +0800

    mac mini

---
+++ Cement Fine butler/IntroductionVC.m
@@ -0,0 +1,49 @@
+//
+//  IntroductionVC.m
+//  Cement Fine butler
+//
+//  Created by 文正光 on 14-4-14.
+//  Copyright (c) 2014年 河南丰博自动化有限公司. All rights reserved.
+//
+
+#import "IntroductionVC.h"
+
+@interface IntroductionVC ()
+
+@end
+
+@implementation IntroductionVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Cement Fine butler/LastIntroductionPanel.h
@@ -0,0 +1,13 @@
+//
+//  LastIntroductionPanel.h
+//  Cement Fine butler
+//
+//  Created by 文正光 on 14-4-14.
+//  Copyright (c) 2014年 河南丰博自动化有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LastIntroductionPanel : UIView
+
+@end
